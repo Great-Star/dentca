764e1aeb6d873c9aa83b8a9ee5b8f796894d5dfe
roy hou <pet0530@outlook.com>
2017-11-10 00:53:06 +1100

    update for runtime compiler issue 1

---
+++ dist/common.chunk.js
@@ -0,0 +1,24 @@
+webpackJsonp(["common"],{
+
+/***/ "../../../../../src/app/core/models/product.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Product; });
+/*
+ * Product model
+ * Detailed info http://guides.spreecommerce.org/developer/products.html
+ * Public API's http://guides.spreecommerce.org/api/products.html
+ */
+var Product = (function () {
+    function Product() {
+    }
+    return Product;
+}());
+
+//# sourceMappingURL=product.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=common.chunk.js.map
